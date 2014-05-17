fa3597c29406720179bef6cf9847e81e3b6eca1b
Aram Baghdasaryan <xquack@gmail.com>
2014-05-17 22:22:34 +0400

    Navigation and category slider

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,16 @@ var App = Class({
 		prev: null,
 		next: null,
 
-		pages: null
+		pages: null,
+		thumbnails: null,
+		categories: null,
 
+		houseUrl: false,
+		vinylUrl: false,
+
+		imgBase: false,
+
+		fenceCategoriesSwiper: false
 	},
 
 	defineVariables: function() {
@@ -21,10 +29,14 @@ var App = Class({
 		this.nav_step2 = $('.nav-step2');
 		this.nav_step3 = $('.nav-step3');
 
-		this.prev = $('.previous');
-		this.next = $('.next');
+		this.prev = $('.previous button');
+		this.next = $('.next button');
 
 		this.pages = $('.page');
+		this.thumbnails = $('#choose-houses').find('.thumbnail');
+		this.categories = $('.fence-categories').find('a');
+
+		this.imgBase = $('.house-container').find('.img-base');
 	},
 
 	initialize: function() {
@@ -32,9 +44,26 @@ var App = Class({
 		this.setStep(1);
 
 		this.bindEvents();
+		this.activateNext(false);
+
+		this.initPlugins();
+	},
+	initPlugins: function() {
+		var catSwipeElement = $('.swiper-container');
+
+		catSwipeElement.css('width', $(document).width());
+
+		this.fenceCategoriesSwiper = catSwipeElement.swiper({
+			freeMode: true,
+			freeModeFluid: true,
+			slidesPerView: 3
+		});
 	},
 	bindEvents: function() {
-		this.prev.on('click', function() {
+		// Move Prev
+		this.prev.on('click', function(e) {
+			e.preventDefault();
+
 			switch (window.app.step) {
 				case 1:
 					window.app.returnToHome();
@@ -55,7 +84,10 @@ var App = Class({
 			}
 		});
 
-		this.next.on('click', function() {
+		// Move Next
+		this.next.on('click', function(e) {
+			e.preventDefault();
+
 			switch (window.app.step) {
 				case 1:
 					window.app.setStep(2);
@@ -72,19 +104,89 @@ var App = Class({
 			}
 		});
 
-		this.nav.on('click', function() {
+		// Move by Navigation
+		this.nav.on('click', function(e) {
+			e.preventDefault();
+
 			var btn = $(this);
 
 			if (!btn.prop('disabled')) {
 				window.app.setStep(parseInt(btn.attr('data-step')));
 			}
 		});
+
+		// Choose House
+		this.thumbnails.on('click', function(e) {
+			e.preventDefault();
+
+			var thumb = $(this);
+
+			if (thumb.hasClass('active')) {
+				window.app.deselectHouse(thumb);
+			} else {
+				window.app.selectHouse(thumb);
+			}
+		});
+
+		// Choose Category
+		this.categories.on('click', function(e) {
+			e.preventDefault();
+
+			window.app.categories.removeClass('active');
+			$(this).addClass('active');
+		});
+	},
+	selectHouse: function(thumb) {
+		this.houseUrl = thumb.attr('data-src');
+		this.thumbnails.removeClass('active');
+		thumb.addClass('active');
+
+		this.activateNext(true);
+	},
+	deselectHouse: function(thumb) {
+		this.houseUrl = false;
+		thumb.removeClass('active');
+
+		this.activateNext(false);
 	},
 	setStep: function(number) {
+		// Unlock Next for next action after prev action
+		this.activateNext(this.step > number);
+
 		this.step = number;
 
 		this.lockPositions();
 		this.showPage(this.step);
+		this.highlightStep(this.step);
+
+		switch (this.step) {
+			case 1:
+				break;
+			case 2:
+				this.initChooseHouse();
+
+				break;
+			case 2:
+				this.initChooseFence();
+
+				break;
+		}
+	},
+	initChooseHouse: function() {
+		this.imgBase.attr('src', this.houseUrl);
+	},
+	initChooseFence: function() {
+
+	},
+	activateNext: function(status) {
+		this.next.prop('disabled', !status);
+	},
+	activatePrev: function(status) {
+		this.prev.prop('disabled', !status);
+	},
+	highlightStep: function(number) {
+		this.nav.removeClass('btn-primary');
+		$('.nav-step' + number).addClass('btn-primary');
 	},
 	showPage: function(number) {
 		this.pages.hide();
