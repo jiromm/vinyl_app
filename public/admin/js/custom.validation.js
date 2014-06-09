0961f077e6a91b16d9ea6962b055b6762d476d81
Aram Baghdasaryan <xquack@gmail.com>
2014-06-09 21:26:44 +0400

    Admin panel front end

---
+++ public/admin/js/custom.validation.js
@@ -0,0 +1,55 @@
+$(function() {
+	$(".parents-form").validate({
+		rules: {
+			phone: {
+				digits: true,
+				required: true,
+				minlength: 8,
+				maxlength: 8
+			},
+			phone_alternative: {
+				digits: true,
+				minlength: 8,
+				maxlength: 8
+			}
+		},
+		highlight: function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		unhighlight: function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+		},
+		errorElement: 'span',
+		errorClass: 'help-block',
+		errorPlacement: function(error, element) {
+			if(element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
+});
+
+$(function() {
+	$(".child-form").validate({
+		rules: {
+
+		},
+		highlight: function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		unhighlight: function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+		},
+		errorElement: 'span',
+		errorClass: 'help-block',
+		errorPlacement: function(error, element) {
+			if(element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
+});
